2c207f887c69512d61abcef764b069cd95b4ed62
db0company <db0company@gmail.com>
2013-07-24 16:58:34 -0700

    Padd matrix

--- sgemm-small.c
+++ sgemm-small.c
@@ -1,35 +1,86 @@
+
 #include <nmmintrin.h>
+#include <strings.h>
+#include <stdio.h>
 
-/* Naive implementation reference */
+#define SPECIAL 36
+#define BLOCKSIZE sizeof(__m128i) / sizeof(float) // should be 4
 
-// void sgemm( int m_a, int n_a, float *A, float *B, float *C ) {
-//   for( int i = 0; i < m_a; i++ ) {
-//     for( int k = 0; k < n_a; k++ ) {
-//       for( int j = 0; j < m_a; j++ ) {
-// 	C[i+j*m_a] += A[i+k*m_a] * B[j+k*m_a];
-//       }
-//     }
-//   }
-// }
+#define A_width  m_a
+#define A_height n_a
 
-/*Header*/
-void padMatrix(float* mat);
-void sgemm36(int m_a, int n_a, float *A, float *B, float *C);
+#define B_width  n_a
+#define B_height m_a
 
+#define C_width  m_a
+#define C_height m_a
 
-/*Functions*/ 
+/* ************************************************************************* */
+/* Tools                                                                     */
+/* ************************************************************************* */
 
-/*	Deals with general case where C = A x B, A is MxN and B is NxM */
-void sgemm( int m_a, int n_a, float *A, float *B, float *C ) {
+#define total(width, height) (width * height)
 
+void error(char * msg) {
+  fprintf(stderr, "[Error] %s\n", msg);
+  exit(EXIT_FAILURE);
 }
 
-/* deals with the special case where matrix A and B are 36x36
-	requires some heavy hand-optimization to achieve 10.5Glop/s*/ 
-void sgemm36(int m_a, int n_a, float *A, float *B, float *C){
-
+/* Call me when *malloc fails */
+void errorAllocation(void) {
+  error("Allocation failure");
 }
 
-/* Pads the matrix so that both dimentions are the nearest multiples of four*/
-void padMatrix(float* mat){
+/* ************************************************************************* */
+/* Matrix Modifications                                                      */
+/* ************************************************************************* */
+
+/* Transform a regular float matrix into a SIMD matrix */
+__m128i load(float * m, int position) {
+  //return _mm_loadu_pd(m + position);
 }
+
+void store(__m128i data, float * m, int position) {
+  //_mm_storeu_pd(m + position, data);
+}
+
+/* Add padding to the matrix so that both dimentions are the nearest
+ * multiples of four to enable register blocking.
+ * __mm_128i allows 4 single precision floating points */
+void padMatrix(int size, float ** m) {
+  if (size % BLOCKSIZE) {
+    if (*m = realloc(*m, (size + (size % BLOCKSIZE)) * sizeof(float)))
+      errorAllocation();
+    bzero(m + size, (size % BLOCKSIZE) * sizeof(float)); /* fill with 0 */
+  }
+}
+
+/* ************************************************************************* */
+/* SGEMM Cases                                                               */
+/* ************************************************************************* */
+
+/* Deals with general case where C = A x B, A is MxN and B is NxM */
+void sgemmRegular(int m_a, int n_a, float * A, float * B, float * C) {
+  __m128i r;
+  padMatrix(total(A_width, A_height), &A);
+  padMatrix(total(B_width, B_height), &B);
+  padMatrix(total(C_width, C_height), &C);
+  for (int i = 0; i < total(C_width, C_height); i += BLOCKSIZE) {
+    // todo: algorithm
+    //r = _mm_add_pd(load(C, i), _mm_mul_pd(load(A, i), load(B, i)));
+    //store(r, C, i);
+  }
+}
+
+/* Deals with the SPECIAL case where matrix A and B are SPECIAL x SPECIAL.
+ * Requires some heavy hand-optimization to achieve 10.5Glop/s */ 
+void sgemmSpecial(float * A, float * B, float * C) {
+  // todo
+}
+
+void sgemm(int m_a, int n_a, float * A, float * B, float * C) {
+  m_a == SPECIAL && n_a == SPECIAL ?
+    sgemmSpecial(A, B, C)
+    : sgemmRegular(m_a, n_a, A, B, C);
+}
+
